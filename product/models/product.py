5d20766f7a26fbaadb41477ea3ae7d93438d5771
hs-2171237-hyunjunshin <livetone2002@gmail.com>
2026-03-15 17:53:10 +0900

    외래키 설정

--- product/models/product.py
+++ product/models/product.py
@@ -4,18 +4,14 @@ class Product(models.Model):
     product_id = models.BigAutoField(db_comment="제품 ID", db_column='product_id', primary_key=True)
 
     #아직 완성 안됐으므로 외래키는 주석처리
-    #store = models.ForeignKey('store.Store', on_delete=models.CASCADE, db_comment="매장 ID", db_column='store_id')
-    #category = models.ForeignKey('product.Category', on_delete=models.CASCADE, db_comment="카테고리 ID", db_column='category_id')
-
-    store_id = models.IntegerField(db_comment="매장 ID", db_column='store_id', null=True)
-    category_id = models.IntegerField(db_comment="카테고리 ID", db_column='category_id', null=True)
+    store_id = models.ForeignKey('store.Store', on_delete=models.CASCADE, db_comment="매장 ID", db_column='store_id')
+    category_id = models.ForeignKey('product.Category', on_delete=models.CASCADE, db_comment="카테고리 ID", db_column='category_id')
     product_name = models.CharField(db_comment="제품 이름", db_column='product_name', max_length=30)
     product_desc = models.TextField(db_comment="제품 설명", db_column='product_desc', null=True)
-    product_stock = models.SmallIntegerField(db_comment="제품 재고", db_column='product_stock',null=True)
+    product_count = models.SmallIntegerField(db_comment="제품 재고", db_column='product_count',null=True)
     product_dis_price = models.IntegerField(db_comment="제품 할인 후 가격", db_column='product_dis_price', null=True)
     product_ori_price = models.IntegerField(db_comment="제품 할인 전 가격", db_column='product_ori_price', null=True)
     product_freshness = models.IntegerField(db_comment="제품 신선도", db_column='product_freshness', null=True)
-
     is_deleted = models.BooleanField(db_comment="삭제 여부", db_column='is_deleted', default=False)
     reg_dt = models.DateTimeField(db_comment="생성 일자", db_column='reg_dt', auto_now_add=True)
 
--- product/models/productImg.py
+++ product/models/productImg.py
@@ -4,11 +4,8 @@ from product.models import Product
 
 
 class ProductImg(models.Model):
-    # 아직 완성 안됐으므로 외래키는 주석처리
-    # product_id = models.ForeignKey('product.Product', on_delete=models.CASCADE, db_comment="매장 ID", db_column='product_id')
-    # img_id = models.ForeignKey('image.Image', on_delete=models.CASCADE, db_comment="카테고리 ID", db_column='img_id')
-    product_id = models.IntegerField(db_comment='제품 ID',db_column='product_id')
-    img_id = models.IntegerField(db_comment='이미지 ID',db_column='img_id')
+    product_id = models.ForeignKey('product.Product', on_delete=models.CASCADE, db_comment="매장 ID", db_column='product_id')
+    img_id = models.ForeignKey('image.Image', on_delete=models.CASCADE, db_comment="카테고리 ID", db_column='img_id')
 
     class Meta:
         db_table = "제품 이미지 테이블"
