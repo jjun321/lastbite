1c5aa44d8155e57a74a11d9f3f46e2b7ea34541e
hs-2171237-hyunjunshin <livetone2002@gmail.com>
2026-03-31 18:31:51 +0900

    refactor:url prefix 정리

--- backend/lastbite/urls.py
+++ backend/lastbite/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("store.urls")),
-    path("", include("product.urls")),
+    path("stores/", include("store.urls")),
+    path("stores/", include("product.urls")),
     path("orders/", include("order.urls")),
     path("cart/", include("cart.urls")),
 ]
--- backend/store/urls.py
+++ backend/store/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import StoreListView, StoreDetailView, StoreHoursView
 
 urlpatterns = [
-    path('stores', StoreListView.as_view(), name='store-list'),
-    path('stores/<int:store_id>', StoreDetailView.as_view(), name='store-detail'),
-    path('stores/<int:store_id>/hours', StoreHoursView.as_view(), name='store-hours'),
+    path('', StoreListView.as_view(), name='store-list'),
+    path('<int:store_id>/', StoreDetailView.as_view(), name='store-detail'),
+    path('<int:store_id>/hours/', StoreHoursView.as_view(), name='store-hours'),
 ]
