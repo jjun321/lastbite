aa930e0c585782a17976b5fafd32dd7a2d8d8996
jjuney <dbjun100@hansung.ac.kr>
2026-03-31 16:13:43 +0900

    feat: main.dart 주석 수정

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/app/app.dart';
 
-/// ------------------------------------------------------------
-/// 파일명: main.dart
-/// 위치: lib/main.dart
-///
-/// 역할:
 /// 앱 실행 시작점
-/// ------------------------------------------------------------
-///
 void main() {
   runApp(const MyApp());
 }
