5afff591a00256b51d2929fdda0efba5f00db598
hs-2171237-hyunjunshin <livetone2002@gmail.com>
2026-04-08 21:22:57 +0900

    feat: 주문내 각 제품에 대한 상세정보

--- backend/order/urls.py
+++ backend/order/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from order.views import OrderCancelView, OrderDetailView, OrderView
+from order.views import OrderCancelView, OrderDetailView, OrderView, OrderItemDetailView
 
 urlpatterns = [
     path('', OrderView.as_view(), name='order-list-create'),
     path('<int:order_id>/', OrderDetailView.as_view(), name='order-detail'),
     path('<int:order_id>/cancel/', OrderCancelView.as_view(), name='order-cancel'),
+    path('<int:order_id>/items/<int:order_product_id>/', OrderItemDetailView.as_view(), name='order-item-detail'),
 ]
