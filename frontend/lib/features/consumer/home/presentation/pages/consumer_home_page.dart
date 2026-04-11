a40096856ad8bb47fe94d72d7a4c36d12e1e53bd
Jawon Yun <dbswkdnjs0321@naver.com>
2026-04-11 16:36:53 +0900

    feat:제보게시판, 제보하기 페이지 제작

--- frontend/lib/features/consumer/home/presentation/pages/consumer_home_page.dart
+++ frontend/lib/features/consumer/home/presentation/pages/consumer_home_page.dart
@@ -3,7 +3,7 @@ import 'package:frontend/features/consumer/home/presentation/pages/home_tab_page
 import 'package:frontend/features/consumer/home/presentation/widgets/bottom_nav_bar.dart';
 import 'package:frontend/features/consumer/mypage/presentation/pages/consumer_mypage.dart';
 import 'package:frontend/features/consumer/order/presentation/pages/order_history_page.dart';
-import 'package:frontend/features/consumer/community/report_board_page.dart';
+import 'package:frontend/features/consumer/board/report_board_page.dart';
 
 /// 소비자 홈 화면의 최상위 Shell
 /// 하단 네비게이션 바를 포함하고, 탭 전환을 관리
