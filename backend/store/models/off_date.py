88615921d7cb68f99eeee62ceb45734b95249d66
hs-2171237-hyunjunshin <livetone2002@gmail.com>
2026-06-14 15:24:49 +0900

    fix: 컬럼명

--- backend/store/models/off_date.py
+++ backend/store/models/off_date.py
@@ -7,7 +7,7 @@ class OffDate(models.Model):
     closed_date_id = models.BigAutoField(db_comment="휴무 테이블 ID", db_column='closed_date_id', primary_key=True)
     user_id = models.ForeignKey(User,on_delete=models.CASCADE,db_comment="유저 ID", db_column='user_id')
     store_id = models.ForeignKey(Store,on_delete=models.CASCADE,db_comment="매장 ID", db_column='store_id')
-    off_dt = models.DateField(db_comment="휴무 예약일", db_column="store_address", blank=False, null=False)
+    off_dt = models.DateField(db_comment="휴무 예약일", db_column="off_dt", blank=False, null=False)
     off_desc = models.TextField(db_comment="휴무 사유", db_column="off_desc", blank=True, null=True )
     reg_dt = models.DateTimeField(db_comment="생성 일자", db_column='reg_dt', auto_now_add=True)
 
